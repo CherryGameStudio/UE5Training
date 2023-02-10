25ca20adbd29981b2c5e7abcf0f33e9b439c7767
Cherry <965491096@qq.com>
2023-02-10 18:52:21 +0800

    动画蓝图模板测试

---
+++ AboutBoneAnimation/Source/AboutBoneAnimation/AnimBlueprintTemplate/CharacterForABPT.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AboutBoneAnimation/Base/MyCharacterBase.h"
+#include "CharacterForABPT.generated.h"
+
+UCLASS()
+class ABOUTBONEANIMATION_API ACharacterForABPT : public AMyCharacterBase
+{
+	GENERATED_BODY()
+	
+};
